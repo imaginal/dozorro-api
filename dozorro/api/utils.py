3c4005ad98bb700f73e85febd3029855106094fd
Volodymyr Flonts <flyonts@gmail.com>
2017-10-27 02:29:00 +0300

    change schema keys

--- dozorro/api/utils.py
+++ dozorro/api/utils.py
@@ -14,7 +14,7 @@ async def load_keyring(app):
         logger.info('Load pubkey {}'.format(fn))
         data = json.loads(open(fn, 'rb').read())
         model = data['envelope']['model']
-        assert model == 'admin/publicKey', 'bad key model'
+        assert model == 'admin/pubkey', 'bad key model'
         payload = data['envelope']['payload']
         owner = payload['owner']
         keyring[owner] = payload
@@ -31,14 +31,14 @@ async def load_schemas(app):
         logger.info('Load comment {}'.format(fn))
         root = json.loads(open(fn, 'rb').read())
         payload = root['envelope']['payload']
-        comment = payload['jsonSchema']
+        comment = payload['schema']
     for fn in glob.glob(path + '/*.json'):
         logger.info('Load schema {}'.format(fn))
         root = json.loads(open(fn, 'rb').read())
         model = data['envelope']['model']
-        assert model == 'admin/jsonSchema', 'bad schema model'
+        assert model == 'admin/schema', 'bad schema model'
         payload = root['envelope']['payload']
-        data = payload['jsonSchema']
+        data = payload['schema']
         if 'definitions' not in data:
             data['definitions'] = comment['definitions']
         model, schema = payload['model'].split('/')

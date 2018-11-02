855d28816f40dc59c7ade322a9842e15d976f3e5
Volodymyr Flyonts <flyonts@ukr.net>
2018-11-02 19:21:06 +0200

    pass config to wsgi via environ API_CONFIG

--- dozorro/api/wsgi.py
+++ dozorro/api/wsgi.py
@@ -1,5 +1,7 @@
+from os import environ
 from asyncio import get_event_loop
 from .main import init_app
 
 loop = get_event_loop()
-app = loop.run_until_complete(init_app(loop))
+conf = environ.get('API_CONFIG')
+app = loop.run_until_complete(init_app(loop, conf))
